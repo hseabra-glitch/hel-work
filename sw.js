aafdd45e625f7f1b18dfc96dd20e0f82b2884fa0
hseabra-glitch <h_seabra@hotmail.com>
2026-05-04 23:08:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,6 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('hellwork').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html'
-      ]);
-    })
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
